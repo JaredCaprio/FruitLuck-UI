1d7bb2c16000fd8f387a24458377db7d6049e40e
JaredCaprio <88853874+JaredCaprio@users.noreply.github.com>
2023-12-27 16:16:58 -0500

    Still learning API routes in Svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,9 @@
-
 <script>
-    // @ts-nocheck
-import Header from "$lib/header.svelte";
-import Footer from "$lib/footer.svelte";
-
+	import Header from '$lib/header.svelte';
+	import Footer from '$lib/footer.svelte';
 </script>
 
-
-<Header/>
+<Header />
 
 <slot />
 
--- src/routes/api/comments/[commentId]/+server.js
+++ src/routes/api/comments/[commentId]/+server.js
@@ -1,8 +1,8 @@
+// @ts-nocheck
 import { json } from "@sveltejs/kit";
 import { comments } from "$lib/comments.js";
 
-// @ts-ignore
-export function POST(RequestEvent) {
+export function GET(RequestEvent) {
   const { params } = RequestEvent;
   const { commentId } = params;
   const comment = comments.find(
@@ -10,4 +10,31 @@ export function POST(RequestEvent) {
   );
 
   return json(comment);
+}
+
+export async function PATCH(requestEvent) {
+  const { params, request } = requestEvent;
+  const { commentId } = params;
+
+  const { text } = await request.json();
+  const comment = comments.find(
+    (comment) => comment.id === parseInt(commentId)
+  );
+  comment.text = text;
+
+  return json(comment);
+}
+
+export async function DELETE(requestEvent) {
+  const { params } = requestEvent;
+  const { commentId } = params;
+
+  const deletedComment = comments.find(
+    (comment) => comment.id === parseInt(commentId)
+  );
+  const index = comments.findIndex(
+    (comment) => comment.id === parseInt(commentId)
+  );
+  comments.splice(index, 1);
+  return json(comments);
 }
